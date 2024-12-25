09240e9b59a3f3f5587a4793ae82ddaa737b5b6b
valiullin.i <valiullininsaf2@gmail.com>
2024-12-26 00:04:26 +0300

    Сделал

--- SchoolSystem/Program.cs
+++ SchoolSystem/Program.cs
@@ -1,3 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+public class School
+{
+    public string SchoolName { get; set; }
+    public List<Classroom> Classrooms { get; set; } = new List<Classroom>();
+    public List<Teacher> Teachers { get; set; } = new List<Teacher>();
+    public List<Staff> StaffMembers { get; set; } = new List<Staff>();
+}
+
+public class Classroom
+{ 
+    public string ClassroomName { get; set; }
+    public Teacher ClassTeacher { get; set; }
+    public List<Student> Students { get; set; } = new List<Student>();
+}    
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Gender { get; set; }
+}
+
+public class Student : Person
+{
+    public string lvl { get; set; } // В каком классе учится (5А, 6В ...)
+}
+
+public class Teacher : Person
+{
+    public string Subject { get; set; }
+}
+
+public class Staff : Person
+{
+    public string JobName { get; set; }  
+}
